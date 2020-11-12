a596231c1d385061438c3959473e7d71df3e769a
baixinping <baixinping@jd.com>
2020-11-12 18:00:06 +0800

    first

---
+++ src/main/java/com/bxp/HelloWord.java
@@ -0,0 +1,12 @@
+package com.bxp;
+
+/**
+ * Author:  baixinping
+ * Date:  2020-11-12
+ * Description：
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
